4ba6cc73918a5de4b444b64d314c310467ca291d
Sasi <dattu2009@yahoo.com>
2024-05-26 08:08:35 +0000

    main tf change for network settings

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "minikube_cluster" {
   provisioner "local-exec" {
     command = <<EOT
-      minikube start --driver=docker --cpus=${var.cpus} --memory=${var.memory} --extra-config=kubelet.networking-pod-cidr=${var.pod_cidr} --extra-config=apiserver.service-cluster-ip-range=${var.service_cidr}
+      minikube start --driver=docker --cpus=${var.cpus} --memory=${var.memory} --driver=docker --extra-config=kubelet.network-plugin=cni --extra-config=kubelet.pod-cidr=${var.pod_cidr} --extra-config=apiserver.service-cluster-ip-range=${var.service_cidr}
     EOT
   }
 
